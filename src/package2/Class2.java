cf3433ad30247d1f166ef1c7c7ea745724a489ed
Sourabh Saha <sourabh.saha46@gmail.com>
2021-07-13 17:44:36 +0530

    Added branch 7 changes

--- src/package2/Class2.java
+++ src/package2/Class2.java
@@ -4,6 +4,7 @@ public class Class2 {
 
 	public static void main(String[] args) {
 		System.out.println("2");
-		System.out.println("2.1");
+		System.out.println("2.1 in branch7");
+		System.out.println("Checking conflicts scenarios");
 	}
 }
